9984a092d7103d81193c49e9fce2a4b0d4850641
WhoisBsa <mbscbsjbs@gmail.com>
2019-01-08 18:09:12 -0300

    PHP

---
+++ exercicios limpos - PHP/unidade_04/inicial.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <!-- Abaixo segue seu nome -->
+        <h1>
+            <?php 
+                echo "Matheus Barbosa Souza"        
+            ?>
+        </h1>
+        <p>
+            <?php 
+                // País de origem
+                echo "Brasil ";
+                echo 2 * 2;
+            ?>
+        </p>
+    </body>
+</html>
